06ae900c43e582e3dd5dfdb5029c146e53798464
Keelin Wheeler <keelinwheeler@gmail.com>
2019-01-01 18:21:54 -0500

    Adjusting cmake

--- PreLoad.cmake
+++ PreLoad.cmake
@@ -1,5 +1,8 @@
-# uncomment to build unix style makefiles
+## uncomment to build unix style makefiles
 # set (CMAKE_GENERATOR "Unix Makefiles" CACHE INTERNAL "" FORCE)
 
-# uncomment to build visual studio solution for windows
+## uncomment to build visual studio solution for windows
 # set (CMAKE_GENERATOR "Visual Studio 15 2017 Win64" CACHE INTERNAL "" FORCE)
+
+option ( DX12_GLOBAL_ENABLE "DirectX12 Enable" OFF )
+option ( DX12_SAMPLE_ENABLE "DirectX12 Sample Enable" OFF )
